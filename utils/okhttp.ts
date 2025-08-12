94bf040d10a8884a0f13ee412aac266eb2ddde6f
e314521 <99789999@qq.com>
2025-08-12 23:36:39 +0800

    修改okhttp

--- utils/okhttp.ts
+++ utils/okhttp.ts
@@ -2,7 +2,7 @@
 import Java from "frida-java-bridge";
 
 function isProbablyUtf8(buffer) {
-    const BufferCls = Java.use("okio.Buffer");
+    const BufferCls = buffer.class;
     const Character = Java.use("java.lang.Character");
 
     const prefix = BufferCls.$new();
@@ -23,15 +23,49 @@ function isProbablyUtf8(buffer) {
 }
 
 
-function hookInterceptor(name) {
+function hookInterceptor(name, buffName, gzipName) {
     Java.perform(function () {
+        console.log("VERSION:", Java.use("okhttp3.OkHttp").VERSION.value)
+
         const CallServerInterceptor = Java.use(name);
-        const BufferCls = Java.use("okio.Buffer");
-        const GzipSource = Java.use("okio.GzipSource");
+        const BufferCls = Java.use(buffName)
+        const GzipSource = Java.use(gzipName);
+        //const ByteBuffer = Java.use("java.nio.ByteBuffer")
+        //const String = Java.use("java.lang.String");xe2
+        var Charset = Java.use("java.nio.charset.Charset")
+        var utf8Charset = Charset.forName("UTF-8")
+        var BufferClsToString = null
+        for (const method of BufferCls.class.getMethods()) {
+            const params = method.getParameterTypes();
+            if (params.length == 1 && params[0].getName() === "java.nio.charset.Charset") {
+                BufferClsToString = method;
+                break;
+            }
+        }
+        if (!BufferClsToString) {
+            console.log("没有找到BufferClsToString");
+            return;
+        }
+        //console.log(defChatset) 
+        //console.log(utf8Charset)
+        //BufferClsToString.overload('int', 'java.lang.String')
+        //console.log(BufferClsToString.overload());
+        /*const RequestBody = Java.use("okhttp3.RequestBody")
+        RequestBody.class.getMethods().forEach(method => {
+            if (method.getName() === "writeTo") {
+                const params = method.getParameterTypes();
+                console.log("参数类型:", Java.use(params[0].getName()).$new()); // 输出如"okio.BufferedSink"
+            }
+        });*/
+
+        //console.log(CallServerInterceptor);
+        //const BufferCls = Java.use("okio.Buffer");
+        //const GzipSource = Java.use("okio.GzipSource");
         // const Utf8Kt = Java.use("okhttp3.logging.Utf8Kt");
+        //const BufferCls = Java.use("okio.Buffer");
+
 
         CallServerInterceptor.intercept.implementation = function (chain) {
-
             const logLines = [];
 
             const request = chain.request();
@@ -55,13 +89,26 @@ function hookInterceptor(name) {
                 curlParts.push(`-H '${name}: ${value}'`);
             }
 
+
             let curlBodyStr = "";
             const requestBody = request.body();
-            if (requestBody != null && !requestBody.isDuplex() && !requestBody.isOneShot()) {
-                const buffer = BufferCls.$new();
-                requestBody.writeTo(buffer);
 
-                if (isProbablyUtf8(buffer)) {
+
+            if (requestBody != null && !requestBody.isDuplex() && !requestBody.isOneShot()) {
+                const ok_buffer = BufferCls.$new();
+                requestBody.writeTo(ok_buffer);
+                try {
+                    var bodyText = BufferClsToString.invoke(ok_buffer, [utf8Charset])
+                    logLines.push("📝 Body:");
+                    const truncated = bodyText.length > 1000 ? bodyText.substring(0, 1000) + "..." : bodyText
+                    logLines.push(truncated);
+                    curlBodyStr = bodyText.replace(/'/g, "'\\''"); // escape single quotes for curl
+                    logLines.push(`--> END ${method} (${requestBody.contentLength()}-byte body)`);
+
+                } catch (error) {
+                    logLines.push(`--> END ${method} (binary ${requestBody.contentLength()}-byte body omitted)`);
+                }
+                /*if (isProbablyUtf8(buffer, buffName)) {
                     logLines.push("📝 Body:");
                     const bodyText = buffer.readUtf8();
                     const truncated = bodyText.length > 1000 ? bodyText.substring(0, 1000) + "..." : bodyText
@@ -70,7 +117,7 @@ function hookInterceptor(name) {
                     logLines.push(`--> END ${method} (${requestBody.contentLength()}-byte body)`);
                 } else {
                     logLines.push(`--> END ${method} (binary ${requestBody.contentLength()}-byte body omitted)`);
-                }
+                }*/
             } else {
                 logLines.push(`--> END ${method}`);
             }
@@ -98,6 +145,7 @@ function hookInterceptor(name) {
                 logLines.push("<-- ❌ HTTP FAILED: " + e);
                 throw e;
             }
+
             const tookMs = (Java.use("java.lang.System").nanoTime() - startNs) / 1000000;
 
             const responseBody = response.body();
@@ -115,13 +163,27 @@ function hookInterceptor(name) {
             }
 
             const encoding = responseHeaders.get("Content-Encoding");
-
             const source = responseBody.source();
             source.request(Java.use("java.lang.Long").MAX_VALUE.value);
-            let buffer = source.buffer();
+            let buffer = source.getBuffer().clone();
             buffer = Java.cast(buffer, BufferCls);
-
+            logLines.push("📄 Body:");
+            logLines.push(buffer)
+            logLines.push(`<-- END HTTP (${buffer.size()}-byte body)`);
             let gzippedLength = null;
+            if (encoding !== null && encoding.toLowerCase() === "gzip") {
+                gzippedLength = buffer.size();
+                const gzipSource = GzipSource.$new(Java.cast(buffer.clone(), BufferCls));
+                const decompressedBuffer = BufferCls.$new();
+                gzipSource.read(decompressedBuffer, Java.use("java.lang.Long").MAX_VALUE.value);
+                buffer = decompressedBuffer;
+            }
+            if (contentLength !== 0) {
+                const bodyText = BufferClsToString.invoke(buffer.clone(), [utf8Charset])
+                logLines.push("📄 Body:");
+                logLines.push(bodyText.length > 1000 ? bodyText.substring(0, 1000) + "..." : bodyText);
+            }
+            /*let gzippedLength = null;
             if (encoding !== null && encoding.toLowerCase() === "gzip") {
                 gzippedLength = buffer.size();
                 const gzipSource = GzipSource.$new(Java.cast(buffer.clone(), BufferCls));
@@ -140,7 +202,7 @@ function hookInterceptor(name) {
                 const bodyText = Java.cast(buffer.clone(), BufferCls).readUtf8();
                 logLines.push("📄 Body:");
                 logLines.push(bodyText.length > 1000 ? bodyText.substring(0, 1000) + "..." : bodyText);
-            }
+            }*/
 
             if (gzippedLength !== null) {
                 logLines.push(`<-- END HTTP (${buffer.size()}-byte, ${gzippedLength}-gzipped-byte body)`);
@@ -202,19 +264,16 @@ function detectOkHttpVersion() {
     });
 }
 
-class okhttp{
-    static hook() {
+class okhttp {
+    static hook(name = "okhttp3.internal.http.CallServerInterceptor", buffName = "okio.Buffer", gzipName = "okio.GzipSource") {
         setImmediate(function () {
-            hookInterceptor("okhttp3.internal.http.CallServerInterceptor")
+            hookInterceptor(name, buffName, gzipName)
         });
     }
 }
-
-
-
-export {okhttp}
-
-
+export { okhttp }
+// okio.Buffer 查找 "Method not decompiled: okio.Buffer.readHexadecimalUnsignedLong():long"  implements nc, mc, Cloneable, ByteChannel
+// GzipSource 查找 "gzip finished without exhausting source"
 // frida -H 127.0.0.1:1234 -F -l okhttp.js
 // frida -H 127.0.0.1:1234 -F -l okhttp.js -o log.txt
 // frida -H 127.0.0.1:1234 -F -l okhttp.js --runtime=v8 --debug
